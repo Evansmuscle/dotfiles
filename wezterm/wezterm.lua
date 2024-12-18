3c74c67a88ec23292d9615b8e1093c56b79bb9aa
Yigit Korkmaz <=>
2024-12-18 10:16:59 +0300

    edit: deletes deprecated font.lua and remove unused comments on wezterm.lua for /wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,48 +1,45 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
--- local font = require 'font'
-
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 -- Color Scheme
-config.color_scheme = 'Dracula'
+config.color_scheme = "Dracula"
 
 -- Font
 -- font.apply_font_cfg(config)
-config.font = wezterm.font {
-	family = 'CaskaydiaCove Nerd Font',
-	harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
-}
+config.font = wezterm.font({
+	family = "CaskaydiaCove Nerd Font",
+	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+})
 config.font_size = 18
 
 -- Inactive pane highlight
 config.inactive_pane_hsb = {
-  saturation = 0.9,
-  brightness = 0.8,
+	saturation = 0.9,
+	brightness = 0.8,
 }
 
 -- Terminal Background Image
-config.window_background_image = '/Users/khan/Pictures/anime_programmer.jpg'
+config.window_background_image = "/Users/khan/Pictures/animu-waifu.jpg"
 config.window_background_image_hsb = {
-  -- Darken the background image by reducing it to 1/3rd
-  brightness = 0.1,
+	-- Darken the background image by reducing it to 1/3rd
+	brightness = 0.3,
 
-  -- You can adjust the hue by scaling its value.
-  -- a multiplier of 1.0 leaves the value unchanged.
-  hue = 1.0,
+	-- You can adjust the hue by scaling its value.
+	-- a multiplier of 1.0 leaves the value unchanged.
+	hue = 1.0,
 
-  -- You can adjust the saturation also.
-  saturation = 1.0,
+	-- You can adjust the saturation also.
+	saturation = 1.0,
 }
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
-config.text_background_opacity = 0.8
+config.text_background_opacity = 0.85
 
 -- and finally, return the configuration to wezterm
 return config
-
